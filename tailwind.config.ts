61c5b88fe7a8b37b5639628264e4131883cf5362
Miquel <edufin215@gmail.com>
2026-03-17 11:59:28 +0100

    feat: add Tailwind with Finomik design tokens

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'finomik-blue': '#0B3064',
+        'finomik-blue-mid': '#114076',
+        'finomik-blue-light': '#5574A7',
+        'finomik-gray': '#8F9EB7',
+        'finomik-gray-light': '#C8D0DD',
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config
